18135ec2fa67e57c411280181ffcb3a24cb14831
naStipe <stephankhlebushkin@gmail.com>
2024-01-09 00:39:26 +0200

    Assignment finished

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+description = document.getElementById("description")
 document.getElementById("actionButton").addEventListener("click", function() {
+    description.style.visibility='visible';
     alert("Button clicked!");
 });
+
+document.getElementById("year").innerHTML = new Date().getFullYear().toString();
+
+
